db744511bbf2dec8530b9375ecc5d0cb2c266382
Talha Ahmed <talha180295@gmail.com>
2019-08-15 17:09:52 +0500

    first commit

---
+++ music_demo/Model/SongModel.swift
@@ -0,0 +1,9 @@
+//
+//  SongModel.swift
+//  music_demo
+//
+//  Created by Talha Ahmed on 05/08/2019.
+//  Copyright © 2019 talha. All rights reserved.
+//
+
+import Foundation
---
+++ music_demo/SongsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SongsViewController.swift
+//  music_demo
+//
+//  Created by Talha Ahmed on 05/08/2019.
+//  Copyright © 2019 talha. All rights reserved.
+//
+
+import UIKit
+
+class SongsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
